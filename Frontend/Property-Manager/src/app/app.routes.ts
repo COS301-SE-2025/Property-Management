5d82af20fff9782a2eee187d2a16e19caaefb6b8
Tafara7 <hwatatafara4@gmail.com>
2025-05-22 13:42:14 +0200

    Initial Register Owner page

--- Frontend/Property-Manager/src/app/app.routes.ts
+++ Frontend/Property-Manager/src/app/app.routes.ts
@@ -1,8 +1,21 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { RegisterOwnerComponent } from './pages/register-owner/register-owner.component';
 
-export const routes: Routes = [{
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login'
+  },
+  {
     path: 'login',
     pathMatch: 'full',
     component: LoginComponent
-}];
+  },
+  {
+    path: 'register-owner',
+    pathMatch: 'full',
+    component: RegisterOwnerComponent
+  }
+];
