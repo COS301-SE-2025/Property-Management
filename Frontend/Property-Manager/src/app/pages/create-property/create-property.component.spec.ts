f7fd98840554145e0b3544055c3aa42179fa48ae
Tafara7 <hwatatafara4@gmail.com>
2025-05-24 10:15:10 +0200

    extra unit tests

--- Frontend/Property-Manager/src/app/pages/create-property/create-property.component.spec.ts
+++ Frontend/Property-Manager/src/app/pages/create-property/create-property.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreatePropertyComponent } from './create-property.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('CreatePropertyComponent', () => {
   let component: CreatePropertyComponent;
@@ -8,7 +9,7 @@ describe('CreatePropertyComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CreatePropertyComponent]
+      imports: [CreatePropertyComponent, ReactiveFormsModule]
     })
     .compileComponents();
 
@@ -19,5 +20,51 @@ describe('CreatePropertyComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have an invalid form when required fields are empty', () => {
+    component.form.patchValue({
+      name: '',
+      address: ''
+    });
+    expect(component.form.invalid).toBeTrue();
+  });
+
+  it('should have a valid form when required fields are filled', () => {
+    component.form.patchValue({
+      name: 'Test Property',
+      address: '123 Main St'
+    });
+    expect(component.form.valid).toBeTrue();
+  });
+
+  it('should patch image value when onFileSelected is called', () => {
+    const file = new File(['dummy content'], 'test.png', { type: 'image/png' });
+    const event = { target: { files: [file] } };
+    component.onFileSelected(event);
+    expect(component.form.value.image).toBe(file);
+  });
+
+  it('should log form value on submit if form is valid', () => {
+    spyOn(console, 'log');
+    component.form.patchValue({
+      name: 'Test Property',
+      address: '123 Main St'
+    });
+    component.onSubmit();
+    expect(console.log).toHaveBeenCalledWith(jasmine.objectContaining({
+      name: 'Test Property',
+      address: '123 Main St'
+    }));
+  });
+
+  it('should not log form value on submit if form is invalid', () => {
+    spyOn(console, 'log');
+    component.form.patchValue({
+      name: '',
+      address: ''
+    });
+    component.onSubmit();
+    expect(console.log).not.toHaveBeenCalled();
   });
 });
