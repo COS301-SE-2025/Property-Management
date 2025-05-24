3d7ce42ab89ae3662a329ee42e0f4a066c331c23
Khomotjo246 <khoomotjomaluleke@email.com>
2025-05-24 14:08:47 +0200

    removed wildcards form imports

--- Backend/property-management/src/main/kotlin/com/example/propertymanagement/controller/TrusteeController.kt
+++ Backend/property-management/src/main/kotlin/com/example/propertymanagement/controller/TrusteeController.kt
@@ -3,7 +3,14 @@ package com.example.propertymanagement.controller
 import com.example.propertymanagement.model.Trustee
 import com.example.propertymanagement.service.TrusteeService
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/trustee")
@@ -19,14 +26,13 @@ class TrusteeController(private val service: TrusteeService) {
         val name: String,
         val email: String,
         val phone: String,
-        val apikey: String,
+        val apikey: String
     )
 
     @PostMapping
     fun createUser(@RequestBody userDto: UserDto): Trustee {
         return service.addUser(userDto.name, userDto.email, userDto.phone, userDto.apikey)
     }
-
 
     @PutMapping("/{id}")
     fun update(@PathVariable id: Long, @RequestBody item: Trustee): Trustee =
