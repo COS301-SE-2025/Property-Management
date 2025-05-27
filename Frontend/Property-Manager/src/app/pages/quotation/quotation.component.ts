328f1da7ebb185ed88b9051d1312e278b4aa329e
Sean <seanmaritz1304@gmail.com>
2025-05-27 21:54:44 +0200

    Fixed linting and backend pipeline creates a postgres service

--- Frontend/Property-Manager/src/app/pages/quotation/quotation.component.ts
+++ Frontend/Property-Manager/src/app/pages/quotation/quotation.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
-import { FileUpload } from 'primeng/fileupload';
 import { CardModule } from 'primeng/card';
 import { CommonModule } from '@angular/common';
 import { MessageService } from 'primeng/api';
