a717436ae7ef993f01a49e09a3538f23622bf333
Sean <seanmaritz1304@gmail.com>
2025-05-27 23:40:00 +0200

    fixed environments.ts

--- Frontend/Property-Manager/src/environments/environments.ts
+++ Frontend/Property-Manager/src/environments/environments.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     cognito: {
-        userPoolId: 'eu-north-us-east-1_XU1g5ApZA',
+        userPoolId: 'us-east-1_XU1g5ApZA',
         userPoolWebClientId: '2fs797fmrmul3t5j6ogi7fhh2u'
  
     }
