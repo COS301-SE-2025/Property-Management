e2da0cc17138f8c861be3279f5998c06f7d3b41e
Khomotjo246 <khoomotjomaluleke@email.com>
2025-05-27 18:50:15 +0200

    added another test to Building Details

--- Backend/property-management/src/test/kotlin/com/example/propertymanagement/controller/BuildingDetailsControllerTest.kt
+++ Backend/property-management/src/test/kotlin/com/example/propertymanagement/controller/BuildingDetailsControllerTest.kt
@@ -13,9 +13,7 @@ import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @WebMvcTest(BuildingDetailsController::class)
 @ContextConfiguration(classes = [com.example.propertymanagement.PropertyManagemnetApplication::class])
