7f2e7494d461ec9b9f5448021ca84f606d18a485
Khomotjo246 <khoomotjomaluleke@email.com>
2025-06-13 23:26:47 +0200

    updated budget service to use uuid

--- Backend/property-management/src/main/kotlin/com/example/propertymanagement/service/BudgetService.kt
+++ Backend/property-management/src/main/kotlin/com/example/propertymanagement/service/BudgetService.kt
@@ -3,10 +3,17 @@ package com.example.propertymanagement.service
 import com.example.propertymanagement.model.Budget
 import com.example.propertymanagement.repository.BudgetRepository
 import org.springframework.stereotype.Service
+import java.util.UUID
 
 @Service
 class BudgetService(
     private val repository: BudgetRepository,
 ) {
-    fun getByBuildingId(buildingId: Int): Budget? = repository.findFirstByBuildingId(buildingId)
+    fun getByBuildingUuid(buildingUuidFk: UUID): Budget? {
+        return repository.findFirstByBuildingUuidFk(buildingUuidFk)
+    }
+
+    fun getByBudgetUuid(budgetUuid: UUID): Budget? {
+        return repository.findByBudgetUuid(budgetUuid)
+    }
 }
