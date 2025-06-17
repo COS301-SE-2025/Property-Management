dc1291660a6fe08e224fbc5eb64920915e48be0e
Sean <seanmaritz1304@gmail.com>
2025-06-17 17:59:31 +0200

    Added register hub

--- Frontend/Property-Manager/src/app/pages/home/house/house-card.component.ts
+++ Frontend/Property-Manager/src/app/pages/home/house/house-card.component.ts
@@ -7,17 +7,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-house-card',
   imports: [CardModule],
-  template: `
-    <div class = "shadow-md rounded-lg border w-75 text-center">
-      <button class = "cursor-pointer" (click)="viewHouse(house().buildingId)">
-        <p-card>
-            <img [src] = "house().image" class="w-60 h-40 mx-auto object-scale-down mt-4 rounded-md" alt="">
-            <p class = "text-lg font-bold mt-4"> {{ house().name }}</p>
-            <p class = "text-gray-600 mb-4"> {{ house().address }}</p>
-        </p-card>
-    </button>
-    </div>
-  `,
+  template: ``,
   styles: ``
 })
 export class HouseCardComponent {
---
+++ Frontend/Property-Manager/src/app/pages/register-hub/register-hub.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-register-hub',
+  imports: [CommonModule],
+  standalone: true,
+  templateUrl: './register-hub.component.html',
+  styles: ``
+})
+export class RegisterHubComponent {
+
+}
