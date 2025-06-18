b6e3aea477a2abfc2dd6c615149f2e870b30a813
Sean <seanmaritz1304@gmail.com>
2025-06-18 12:09:46 +0200

    Revert "Added register hub"
    Removed mock test data sql file

--- Backend/property-management/migrations/V3__Mock_data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO inventoryitem(name, unit, quantity_in_stock, building_id) VALUES('Grey paint 5L', 23, 3, 3);
- INSERT INTO inventoryitem(name, unit, quantity_in_stock, building_id) VALUES('Light bulbs', 12, 6, 3);
-INSERT INTO inventoryitem(name, unit, quantity_in_stock, building_id) VALUES('PVC pipes', 10, 25, 3);
- INSERT INTO inventoryitem(name, unit, quantity_in_stock, building_id) VALUES('Concrete bricks', 50, 8, 4);
-INSERT INTO inventoryitem(name, unit, quantity_in_stock, building_id) VALUES('Ceramic tiles (12x12)', 20, 15, 4);
-INSERT INTO inventoryitem(name, unit, quantity_in_stock, building_id) VALUES('Copper wire (1mm 11m)', 1, 6, 4);
-INSERT INTO budget(budget_id, building_id, year, total_budget, maintenance_budget, inventory_budget, inventory_spent, maintenance_spent) VALUES(1, 3, 2025, 20000, 10000, 10000, 8000, 5000);
-INSERT INTO budget(budget_id, building_id, year, total_budget, maintenance_budget, inventory_budget, inventory_spent, maintenance_spent) VALUES(2, 4, 2025, 10000, 4000, 6000, 2000, 3000);
