aed3ac9a6a13ad509cf5215fe530247ba9b3967a
Sean <seanmaritz1304@gmail.com>
2025-06-18 11:57:12 +0200

    Fixed docker container breaking

--- Backend/property-management/src/main/kotlin/com/example/propertymanagement/controller/ImageController.kt
+++ Backend/property-management/src/main/kotlin/com/example/propertymanagement/controller/ImageController.kt
@@ -5,12 +5,7 @@ import com.example.propertymanagement.repository.ImageRepository
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 import software.amazon.awssdk.core.sync.RequestBody
 import software.amazon.awssdk.services.s3.S3Client
@@ -23,7 +18,7 @@ class ImageController(
     val s3Client: S3Client,
     val imageRepository: ImageRepository,
 ) {
-    @Value("\${aws.bucket-name}")
+    @Value("\${aws.bucket-name:default-bucket}")
     lateinit var bucketName: String
 
     @PostMapping("/upload", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
