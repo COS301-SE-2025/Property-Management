42894cb5af85bf60254c3e63477ff277be9cc772
K.Taole(u23538318) <karabelotaole04@gmail.com>
2025-06-18 09:26:50 +0200

    Fixing docker database setup 2

--- Backend/property-management/migrations/V5__new_implementation_of_trustees.sql
+++ Backend/property-management/migrations/V5__new_implementation_of_trustees.sql
@@ -1,3 +1,14 @@
+
+ALTER TABLE trustee
+ADD COLUMN trustee_uuid UUID DEFAULT gen_random_uuid();
+
+UPDATE trustee SET trustee_uuid = gen_random_uuid()
+WHERE trustee_uuid IS NULL;
+
+ALTER TABLE trustee
+ALTER COLUMN trustee_uuid SET NOT NULL,
+ADD CONSTRAINT trustee_trustee_uuid_unique UNIQUE (trustee_uuid);
+
 CREATE TABLE buildingtrustee (
     building_uuid UUID NOT NULL,
     trustee_uuid UUID NOT NULL,
