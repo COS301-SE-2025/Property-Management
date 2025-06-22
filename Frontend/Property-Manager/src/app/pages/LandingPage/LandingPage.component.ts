27c6c1db495855211525a664f2a22109d2255197
ThabisoXavierMncube <u22617362@tuks.co.za>
2025-06-22 21:13:30 +0200

    removing unnecessary modules

--- Frontend/Property-Manager/src/app/pages/LandingPage/LandingPage.component.ts
+++ Frontend/Property-Manager/src/app/pages/LandingPage/LandingPage.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { CardModule } from 'primeng/card';
+
 import { ButtonModule } from 'primeng/button';
 import { RouterLink} from '@angular/router';
 import { HeaderComponent } from "../../components/header/header.component";
-import { DividerModule } from 'primeng/divider';
+
 @Component({
     selector: 'app-landingpage',
-    imports: [CardModule, ButtonModule, RouterLink, HeaderComponent, DividerModule],
+    imports: [ ButtonModule, RouterLink, HeaderComponent, ],
     standalone: true,
     templateUrl: `./Landingpage.component.html`,
     styles: ``,
--- Frontend/Property-Manager/src/app/pages/reset-password/reset-password.component.ts
+++ Frontend/Property-Manager/src/app/pages/reset-password/reset-password.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { CardModule } from 'primeng/card';
+
 import { ButtonModule } from 'primeng/button';
 import { RouterLink} from '@angular/router';
 import { HeaderComponent } from "../../components/header/header.component";
-import { DividerModule } from 'primeng/divider';
+
 import { FormsModule } from '@angular/forms';
 @Component({
     selector: 'app-resetpage',
-    imports: [CardModule, ButtonModule, RouterLink, HeaderComponent, DividerModule, FormsModule],
+    imports: [ButtonModule, RouterLink, HeaderComponent,  FormsModule],
     standalone: true,
     templateUrl: `./reset-password.component.html`,
     styles: ``,
