35d3fc5f91d3181435814f22a4cc671900bc90f1
ThabisoXavierMncube <u22617362@tuks.co.za>
2025-06-22 02:32:21 +0200

    landing page

--- Frontend/Property-Manager/src/app/pages/LandingPage/LandingPage.component.ts
+++ Frontend/Property-Manager/src/app/pages/LandingPage/LandingPage.component.ts
@@ -3,9 +3,10 @@ import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { RouterLink} from '@angular/router';
 import { HeaderComponent } from "../../components/header/header.component";
+import { DividerModule } from 'primeng/divider';
 @Component({
     selector: 'app-landingpage',
-    imports: [CardModule, ButtonModule, RouterLink, HeaderComponent],
+    imports: [CardModule, ButtonModule, RouterLink, HeaderComponent, DividerModule],
     standalone: true,
     templateUrl: `./Landingpage.component.html`,
     styles: ``,
