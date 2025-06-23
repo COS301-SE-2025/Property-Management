4bbad23166585f1ae4213790eca4cab6c4591ee4
ThabisoXavierMncube <u22617362@tuks.co.za>
2025-06-23 20:55:57 +0200

    edited quotation page

--- Frontend/Property-Manager/src/app/pages/quotation/quotation.component.ts
+++ Frontend/Property-Manager/src/app/pages/quotation/quotation.component.ts
@@ -6,9 +6,10 @@ import { CardModule } from 'primeng/card';
 import { CommonModule } from '@angular/common';
 import { MessageService } from 'primeng/api';
 import { ToastModule } from 'primeng/toast';
-import { FileUploadEvent } from 'primeng/fileupload';
+import { FileUpload } from 'primeng/fileupload';
 import { ApiService } from '../../services/api.service';
 import { Router } from '@angular/router';
+import { HeaderComponent } from "../../components/header/header.component";
 
 @Component({
   selector: 'app-quotation',
@@ -19,69 +20,28 @@ import { Router } from '@angular/router';
     ButtonModule,
     CardModule,
     CommonModule,
-    ToastModule
+    ToastModule,
+    HeaderComponent,
+    FileUpload
   ],
   providers: [MessageService],
   templateUrl: `./quotation.component.html`,
   styles: [``],
 })
 export class QuotationComponent {
-  quotation = {
-    contractorName: '',
-    address: '',
-    profession: '',
-    email: '',
-    phone: '',
-    amount: ''
-  };
-  previewUrl: string | null = null;
-  constructor(private messageService: MessageService, private apiservice: ApiService, private router: Router) {}
+  IssueDate = '';
+  expirationDate = '';
+  quoteNo = '';
+  totalAmount = '';
 
-    onUpload(event: FileUploadEvent) {
-  const file = event.files?.[0];
-  if (file) {
-    const reader = new FileReader();
-    reader.onload = (e: ProgressEvent<FileReader>) => {
-      this.previewUrl = e.target && e.target.result ? e.target.result as string : null;
-    };
-    reader.readAsDataURL(file);
+  constructor(private messageService: MessageService) {}
+
+  onUpload(event: any) {
+    console.log('Uploaded files:', event.files);
+    this.messageService.add({ 
+      severity: 'info', 
+      summary: 'Success', 
+      detail: 'File Uploaded with Basic Mode' 
+    });
   }
-
-  this.messageService.add({
-    severity: 'info',
-    summary: 'Success',
-    detail: 'File Uploaded with Basic Mode'
-  });
-}
-
-  submitQuotation() {
-    this.apiservice.getAllContractors().subscribe((response) => {
-    const nameToFind = this.quotation.contractorName.toLowerCase();
-    let found = false;
-    const cleaned = this.quotation.amount.replace(/[^0-9.-]+/g, "");
-
-    for (const contractor of response) {
-      console.log(contractor?.name);
-      if (
-        contractor?.name &&
-        contractor.name.toLowerCase() === nameToFind
-      ) {
-        console.log("Matching Contractor ID:", contractor.contractorId);
-        found = true;
-        this.apiservice.addQuote(101, contractor.contractorId, Number(cleaned) ,new Date("2025-05-27"), "Mainteneance").subscribe((response2) => {
-            alert('Your Quote was sucessfuly sent, ID:' + response2.quote_id);
-        });
-        console.log();
-        alert('Your Quote was sucessfuly sent');
-        this.router.navigate(['/contractorHome'])
-      }
-    }
-
-    if (!found) {
-      alert("No contractor found with the name:"+ nameToFind);
-    }
-  });
-
-}
-
 }
