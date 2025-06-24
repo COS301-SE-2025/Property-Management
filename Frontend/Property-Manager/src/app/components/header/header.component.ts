bfda73328a526b4d5c3a115828adb315ad39bffb
ThabisoXavierMncube <u22617362@tuks.co.za>
2025-06-24 04:09:41 +0200

    Refactor header to use external HTML template and update icons
    Moved the header component's template to a separate HTML file for better maintainability. Updated the profile and settings SVG icons with new designs.

--- Frontend/Property-Manager/src/app/components/header/header.component.ts
+++ Frontend/Property-Manager/src/app/components/header/header.component.ts
@@ -6,34 +6,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   imports: [CommonModule],
-  template: `
-    <div class = "flex flex-col">
-      <!-- Name -->
-      <div class = "flex flex-row justify-between items-center px-4 py-2"> 
-        <p class = "font-medium text-xl">Property Manager</p>
-
-        <div class = "flex flex-row items-center">
-          <div class = "relative">
-            <button class = "cursor-pointer pr-3"> 
-              <img src= "assets/icons/settings.svg" class="w-9 h-9" alt=""> 
-            </button>
-            <button class = "cursor-pointer" (click)="dropDown()">
-              <img src= "assets/icons/profile.svg" class="w-9 h-9" alt="">
-            </button>
-
-            <div *ngIf="dropDownOpen" class= "absolute right-1 top-14 bg-white border rounded shadow-md w-22 z-50">
-              <button (click)="signOut()" class= "block text-red-500 w-full text-left px-3 py-1 hover:bg-gray-100 cursor-pointer">
-                Sign out
-              </button>            
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <!-- bar -->
-      <div class = "propertyYellow-bg h-4 w-full"></div>
-    </div>
-  `,
+  templateUrl: `./header.component.html`,
   styles: ''
 })
 export class HeaderComponent {
