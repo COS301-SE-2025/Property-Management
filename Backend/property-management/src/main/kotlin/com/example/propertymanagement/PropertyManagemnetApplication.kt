1565afd2c3286e72594da55634c45f9b1e6577dd
KarabeloTaole <karabelotaole04@gmail.com>
2025-06-25 18:20:54 +0200

    Fixing build issues 3.0

--- Backend/property-management/src/main/kotlin/com/example/propertymanagement/PropertyManagemnetApplication.kt
+++ Backend/property-management/src/main/kotlin/com/example/propertymanagement/PropertyManagemnetApplication.kt
@@ -3,7 +3,6 @@ package com.example.propertymanagement
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@ActiveProfiles("test")
 @SpringBootApplication
 class PropertyManagemnetApplication
 
