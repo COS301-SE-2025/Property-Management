cb96ad92889d5bb5f86025bdc86bf1bd459748a1
KarabeloTaole <karabelotaole04@gmail.com>
2025-06-25 14:36:39 +0200

    Getting the new Database stuff

--- Backend/property-management/migrations/V8__MaintenanceTask_changes.sql
+++
@@ -1,8 +0,0 @@
--- change column type
-ALTER TABLE maintenancetask 
-    ALTER COLUMN scheduled_date TYPE timestamp with time zone;
--- change column name
-ALTER TABLE maintenancetask 
-    RENAME COLUMN scheduled_date TO scheduled_date;
--- Change column comment
-COMMENT ON COLUMN maintenancetask.scheduled_date IS 'comment';
