331af49987555a0e666d9dcdf6b4ff441aab448c
Sean <seanmaritz1304@gmail.com>
2025-06-26 10:50:07 +0200

    Fixed linting in backend

--- Backend/property-management/src/main/kotlin/com/example/propertymanagement/controller/ImageController.kt
+++ Backend/property-management/src/main/kotlin/com/example/propertymanagement/controller/ImageController.kt
@@ -5,7 +5,12 @@ import com.example.propertymanagement.repository.ImageRepository
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 import software.amazon.awssdk.core.sync.RequestBody
 import software.amazon.awssdk.services.s3.S3Client
