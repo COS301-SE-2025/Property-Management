93746ac00196bd03b574acdef71440ba885684ee
Khomotjo246 <khoomotjomaluleke@email.com>
2025-06-26 02:07:18 +0200

    fixed linting

--- Backend/property-management/src/test/kotlin/com/example/propertymanagement/controller/BodyCoporateControllerTest.kt
+++ Backend/property-management/src/test/kotlin/com/example/propertymanagement/controller/BodyCoporateControllerTest.kt
@@ -16,8 +16,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import java.math.BigDecimal
 import java.util.UUID
 
