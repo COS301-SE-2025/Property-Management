c1bff469e5673b9d956be10fe0dd66ea8171ca3d
Khomotjo246 <khoomotjomaluleke@email.com>
2025-07-13 18:13:15 +0200

    added invite repository

---
+++ Backend/property-management/src/main/kotlin/com/example/propertymanagement/repository/TrusteeBodyCorporateInviteRepository.kt
@@ -0,0 +1,14 @@
+package com.example.propertymanagement.repository
+
+import com.example.propertymanagement.model.TrusteeBodyCorporateInvite
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.UUID
+
+@Repository
+interface TrusteeBodyCorporateInviteRepository : JpaRepository<TrusteeBodyCorporateInvite, UUID> {
+
+    fun findByTrusteeUuidAndCoporateUuid(trusteeUuid: UUID, coporateUuid: UUID): TrusteeBodyCorporateInvite?
+
+    fun findAllByTrusteeUuid(trusteeUuid: UUID): List<TrusteeBodyCorporateInvite>
+}
