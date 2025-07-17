4a38f7b68e0611c27495a1183da84b112788da89
Tafara7 <hwatatafara4@gmail.com>
2025-07-17 15:59:39 +0200

    Token setup changes

--- Frontend/Property-Manager/src/app/services/token-util.service.ts
+++ Frontend/Property-Manager/src/app/services/token-util.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
 import { jwtDecode } from 'jwt-decode';
 
+interface JwtPayload {
+  'cognito:groups'?: string[];
+  [key: string]: unknown;
+}
+
 @Injectable({ providedIn: 'root' })
 export class TokenUtilService {
   getUserGroups(token: string): string[] {
     try {
-      const decoded: any = jwtDecode(token);
+      const decoded: JwtPayload = jwtDecode(token);
       return decoded['cognito:groups'] || [];
     } catch {
       return [];
