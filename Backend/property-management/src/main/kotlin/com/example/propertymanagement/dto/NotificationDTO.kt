c58a646b236a666f90e00fc6028204996a36df10
Khomotjo246 <khoomotjomaluleke@email.com>
2025-07-22 19:35:11 +0200

    Notification DTO

---
+++ Backend/property-management/src/main/kotlin/com/example/propertymanagement/dto/NotificationDTO.kt
@@ -0,0 +1,16 @@
+package com.example.propertymanagement.dto
+
+import java.time.LocalDateTime
+import java.util.UUID
+
+data class NotificationDTO (
+    val notificationUuid : UUID,
+    val createdAt : LocalDateTime,
+    val notificationType: String,
+    val message: String,
+    val isRead: Boolean,
+    val relatedTaskUuid: UUID? = null,
+    val relatedQuoteUuid: UUID? = null,
+    val relatedSessionUuid: UUID? = null,
+    val relatedInviteUuid: UUID? = null,
+)
