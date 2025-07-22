4e1ca4b855dc4b5f02520907cc7a31a7ba856698
Khomotjo246 <khoomotjomaluleke@email.com>
2025-07-22 19:11:27 +0200

    Notofication Table model

---
+++ Backend/property-management/src/main/kotlin/com/example/propertymanagement/model/Notification.kt
@@ -0,0 +1,34 @@
+package com.example.propertymanagement.model
+
+import java.time.LocalDateTime
+import java.util.UUID
+import jakarta.persistence.Column
+import jakarta.persistence.Table
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+
+@Entity
+@Table(name = "notification")
+data class Notification (
+    @Id
+    @Column(name = "notification_uuid", unique = true)
+    val notificationUuid: UUID = UUID.randomUUID(),
+    @Column(name = "created_at")
+    val createdAt: LocalDateTime,
+    @Column(name = "notification_type")
+    val notificationType: String,
+    @Column(name = "message" )
+    val message: String,
+    @Column(name = "user_uuid uuid")
+    val userUuid: UUID,
+    @Column(name = "is_read")
+    val isRead: Boolean,
+    @Column(name = "related_task_uuid")
+    val relatedTaskUuid: UUID? = null,
+    @Column(name = "related_quote_uuid")
+    val relatedQuoteUuid: UUID? = null,
+    @Column(name = "related_session_uuid")
+    val relatedSessionUuid: UUID? = null,
+    @Column(name = "related_invite_uuid")
+    val relatedInviteUuid: UUID? = null,
+)
