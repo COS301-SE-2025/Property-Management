c4d10a11549121855cb148d7e740b992e63c00b4
ThabisoXavierMncube <u22617362@tuks.co.za>
2025-08-02 21:42:39 +0200

    changing tabs to fit with usertype mobile app

--- Frontend/mobile-app/PropertyManager/src/app/components/tab/tab.component.ts
+++ Frontend/mobile-app/PropertyManager/src/app/components/tab/tab.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
 import { IonTabBar, IonTabButton, IonIcon, IonTabs } from '@ionic/angular/standalone'
+import { ApiService } from 'shared';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-tab',
-  imports: [IonTabBar, IonTabButton, IonIcon, IonTabs],
+  imports: [IonTabBar, IonTabButton, IonIcon, IonTabs, NgIf],
   templateUrl: './tab.component.html',
   styles: ``,
 })
 export class TabComponent {
-
+  private api = inject(ApiService);
+  type: string ="";
+  constructor() {
+    this.type = this.api.getCookieValue('userType') || '';
+  }
 }
