beac087a3f142e2bab630daad70733dd634aceb2
Sean-Mp <seanmaritz1304@gmail.com>
2025-08-15 16:46:27 +0200

    Updated mobile app

--- Frontend/mobile-app/PropertyManager/src/app/pages/home/home.component.ts
+++ Frontend/mobile-app/PropertyManager/src/app/pages/home/home.component.ts
@@ -8,10 +8,11 @@ import { TabComponent } from "src/app/components/tab/tab.component";
 import { IonItem, IonContent } from "@ionic/angular/standalone";
 import { HouseComponent } from './house/house.component';
 import { ThemeService } from 'src/app/services/theme.service';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 
 @Component({
   selector: 'app-home',
-  imports: [IonItem, HeaderComponent, TabComponent, CommonModule, FormsModule, HouseComponent, IonContent],
+  imports: [IonItem, HeaderComponent, TabComponent, CommonModule, FormsModule, HouseComponent, IonContent, ProgressSpinnerModule],
   templateUrl: './home.component.html',
   styles: ``
 })
@@ -20,6 +21,7 @@ export class HomeComponent implements OnInit{
   private houseService = inject(HousesService);
   houses = this.houseService.houses;
   darkMode = false;
+  loading = false;
 
   constructor(private storage: StorageService, private router: Router, private theme: ThemeService) { 
     
@@ -27,11 +29,13 @@ export class HomeComponent implements OnInit{
 
   async ngOnInit()
   {
+    this.loading = true;
     const id = await this.storage.get('trusteeId');
     // await this.storage.set("trusteeId", id);
     this.houseService.loadHouses(id);
 
     this.theme.darkMode$.subscribe(mode => this.darkMode = mode);
+    this.loading = false;
   }
 
   RouteToCreateProperty()
