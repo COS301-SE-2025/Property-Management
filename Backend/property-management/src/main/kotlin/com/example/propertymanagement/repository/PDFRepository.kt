c492c6154dcf39ba8949d390b027b81d36b6a476
KarabeloTaole <karabelotaole04@gmail.com>
2025-08-16 10:32:28 +0200

    Pdf upload connected to contractor

--- Backend/property-management/src/main/kotlin/com/example/propertymanagement/repository/PDFRepository.kt
+++ Backend/property-management/src/main/kotlin/com/example/propertymanagement/repository/PDFRepository.kt
@@ -2,7 +2,16 @@ package com.example.propertymanagement.repository
 
 import com.example.propertymanagement.model.PDFMeta
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import java.util.Optional
+import java.util.UUID
 
-@Repository
-interface PDFRepository : JpaRepository<PDFMeta, String>
+
+interface PDFRepository : JpaRepository<PDFMeta, String>{
+
+    @Query("SELECT p FROM pdf_meta p WHERE p.contractor_uuid = :cUuid")
+    fun findAllByContractorUuid(
+        @Param("cUuid") cUuid: UUID,
+    ): List<PDFMeta>
+}
