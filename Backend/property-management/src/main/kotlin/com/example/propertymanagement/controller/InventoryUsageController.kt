14cb7451a83c9bab934db9e469f2ff3c99f06476
Khomotjo246 <khoomotjomaluleke@email.com>
2025-08-17 15:32:16 +0200

    f9ixed failing test

--- Backend/property-management/src/main/kotlin/com/example/propertymanagement/controller/InventoryUsageController.kt
+++ Backend/property-management/src/main/kotlin/com/example/propertymanagement/controller/InventoryUsageController.kt
@@ -12,7 +12,6 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
@@ -27,7 +26,6 @@ import java.util.UUID
 
 @RestController
 @RequestMapping("/api/inventory-usage")
-@CrossOrigin(origins = ["*"])
 class InventoryUsageController(
     private val inventoryUsageService: InventoryUsageService,
 ) {
