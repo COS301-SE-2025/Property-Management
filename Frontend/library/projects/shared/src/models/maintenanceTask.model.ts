1a5299fef6102824788e4b325e7dde00332225f1
ThabisoXavierMncube <u22617362@tuks.co.za>
2025-08-17 23:16:15 +0200

    updating maintenance model and quote model

--- Frontend/library/projects/shared/src/models/maintenanceTask.model.ts
+++ Frontend/library/projects/shared/src/models/maintenanceTask.model.ts
@@ -1,6 +1,7 @@
 export interface MaintenanceTask{
     [x: string]: any;
     uuid: string;
+    taskUuid?: string;
     title: string;
     des: string;
     status: string;
@@ -11,6 +12,7 @@ export interface MaintenanceTask{
     tuuid: string;
     cuuid?: string;
     img?: string; 
+    imageUuid?: string; 
     createdByUuid?: string | null;
     priority?: string;
 }
