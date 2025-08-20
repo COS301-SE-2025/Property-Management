c04ec1b77e4cdf7062ff3d9ac78c358b1f0c837f
Sean-Mp <seanmaritz1304@gmail.com>
2025-08-20 10:27:27 +0200

    Fixed frontend unit tests

--- Frontend/library/projects/shared/src/services/api/Task api/task-api.service.spec.ts
+++ Frontend/library/projects/shared/src/services/api/Task api/task-api.service.spec.ts
@@ -263,8 +263,14 @@ describe('TaskApiService', () => {
   describe('getQuoteFromTaskId', () => {
     it('should get quotes for a task', () => {
       const mockQuotes: Quote[] = [
-        { uuid: 'quote-1', t_uuid: 'task-123', amount: 100, status: 'PENDING', c_uuid: '1', submitted_on: 1, doc: '' },
-        { uuid: 'quote-2', t_uuid: 'task-123', amount: 150, status: 'APPROVED', c_uuid: '2', submitted_on: 1, doc: '' }
+        {
+          uuid: 'quote-1', t_uuid: 'task-123', amount: 100, status: 'PENDING', c_uuid: '1', submitted_on: 1, doc: '',
+          expiry_date: ''
+        },
+        {
+          uuid: 'quote-2', t_uuid: 'task-123', amount: 150, status: 'APPROVED', c_uuid: '2', submitted_on: 1, doc: '',
+          expiry_date: ''
+        }
       ];
 
       service.getQuoteFromTaskId('task-123').subscribe(quotes => {
