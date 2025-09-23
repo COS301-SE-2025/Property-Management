85cf6158dd262e787cb911594796717a8cc33271
Sean-Mp <seanmaritz1304@gmail.com>
2025-09-23 10:21:49 +0200

    Fixed bug where public contractor data wouldnt show

--- Frontend/property-manager/src/app/app.routes.ts
+++ Frontend/property-manager/src/app/app.routes.ts
@@ -149,7 +149,7 @@ export const routes: Routes = [
   },
   {
     path: 'contractorDetails/:contractorId/:source',
-    canActivate: [authGuard(['bodyCorporate'])],
+    canActivate: [authGuard(['bodyCorporate', 'trustee'])],
     pathMatch: 'full',
     component: ContractorDetailsComponent
   },
