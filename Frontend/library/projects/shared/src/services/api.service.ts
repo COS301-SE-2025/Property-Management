3d29d0f35be154d248d201ebb3c5ee028d38b27b
ThabisoXavierMncube <u22617362@tuks.co.za>
2025-09-25 23:47:28 +0200

    reset password

--- Frontend/library/projects/shared/src/services/api.service.ts
+++ Frontend/library/projects/shared/src/services/api.service.ts
@@ -258,6 +258,22 @@ async uploadPDF(file: File, uuid: string, type: string): Promise<void> {
       withCredentials: true
     });
   }
+  resetTrusteePasswordRequest(email: string): Observable<any> {
+  const body = { email: email };
+  return this.http.post(`${this.url}/trustee/auth/password-reset-request`, body, {
+    withCredentials: true
+  });
+}
 
+confirmTrusteeResetPassword(email: string, confirmationCode: string, newPassword: string): Observable<any> {
+  const body = {
+    email: email,
+    confirmationCode: confirmationCode,
+    newPassword: newPassword
+  };
+  return this.http.post(`${this.url}/trustee/auth/password-reset-confirm`, body, {
+    withCredentials: true
+  });
+}
 
 }
