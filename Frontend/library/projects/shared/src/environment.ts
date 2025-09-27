b40930ffbf5684c320ec6eb390b6216cf2b7c079
Sean-Mp <seanmaritz1304@gmail.com>
2025-09-27 13:26:32 +0200

    changed from scp to rsync

--- Frontend/library/projects/shared/src/environment.ts
+++ Frontend/library/projects/shared/src/environment.ts
@@ -1,6 +1,6 @@
 export const environmentMobile = {
   production: true,
   apiUrl : 'https://api.property-management.live/api',
-  wowFactorUrl: 'http://localhost:5000'
+  wowFactorUrl: 'https://api.property-management.live/api/forecast'
 };
 
