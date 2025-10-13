2b6d59b3ba076bc36cc8f210acd213549dac1be0
Khomotjo246 <khoomotjomaluleke@email.com>
2025-10-13 23:44:06 +0200

    updated the landing page

--- Backend/property-management/migrations/V26_add_contrcator_type_and_max_budget.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE maintenancetask
-ADD COLUMN max_budget DECIMAL(10,2) DEFAULT NULL;
-
-CREATE TABLE contractor_services (
-    contractor_uuid uuid NOT NULL,
-    service varchar(255) NOT NULL,
-    CONSTRAINT fk_contractor_services_contractor FOREIGN KEY (contractor_uuid) REFERENCES contractor(contractor_uuid) ON DELETE CASCADE
-);
-CREATE INDEX idx_contractor_services_service ON contractor_services (service);
--- Frontend/property-manager/src/app/app.routes.ts
+++ Frontend/property-manager/src/app/app.routes.ts
@@ -30,6 +30,9 @@ import { TaskDetailsComponent } from './pages/task-details/task-details.componen
 import { NotificationsComponent } from './pages/notifications/notifications.component';
 import { RestConfirmComponent } from './pages/reset-confirm/reset-confirm.component';
 import { InventoryApprovalQueueComponent } from './pages/inventory-approval-queue/inventory-approval-queue.component';
+import { LandingheaderComponent } from './pages/LandingPage/landingheader/landingheader.component';
+import { LandingfooterComponent } from './pages/LandingPage/landingfooter/landingfooter.component';
+import { OurteamComponent } from './pages/LandingPage/ourteam/ourteam.component';
 
 
 export const routes: Routes = [
@@ -212,5 +215,20 @@ export const routes: Routes = [
     path: 'notifications',
     pathMatch: 'full',
     component: NotificationsComponent
-  }
+  },
+   {
+    path: 'landing-header',
+    pathMatch: 'full',
+    component: LandingheaderComponent
+  },
+    {
+      path: 'landing-footer',
+      pathMatch: 'full',
+      component: LandingfooterComponent
+  },
+    {
+      path: 'our-team',
+      pathMatch: 'full',
+      component: OurteamComponent
+    }
 ];
