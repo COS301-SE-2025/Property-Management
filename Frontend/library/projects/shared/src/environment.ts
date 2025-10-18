bc75a891253c48e2715c38628e007dba0c61da1b
KarabeloTaole <karabelotaole04@gmail.com>
2025-10-18 11:18:29 +0200

    Uploading the new build to ec2

--- Frontend/library/projects/shared/src/environment.ts
+++ Frontend/library/projects/shared/src/environment.ts
@@ -1,7 +1,7 @@
 export const environmentMobile = {
   production: true,
-  // apiUrl : 'https://api.property-management.live/api',
-  // wowFactorUrl: 'https://api.property-management.live/api/forecast'
-  apiUrl : 'http://localhost:8080/api',
-  wowFactorUrl: 'http://localhost:5000'
+  apiUrl : 'https://api.property-management.live/api',
+  wowFactorUrl: 'https://api.property-management.live/api/forecast'
+  // apiUrl : 'http://localhost:8080/api',
+  // wowFactorUrl: 'http://localhost:5000'
 };
